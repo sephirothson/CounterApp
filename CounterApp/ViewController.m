1a48f81a6f0ef2675cbaea64dc23f6482479ee45
sephirothson <sephirothson@163.com>
2014-11-06 14:48:56 +0800

    Initial Commit

---
+++ CounterApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CounterApp
+//
+//  Created by  sephiroth on 14/11/6.
+//  Copyright (c) 2014年 sephiroth. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
